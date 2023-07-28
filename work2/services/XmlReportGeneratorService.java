c927acecb1fffba50132c988b049fcb8ee43a1cc
arthursae <118846805+arthursae@users.noreply.github.com>
2023-07-28 20:16:43 +0500

    Create XmlReportGeneratorService.java

---
+++ work2/services/XmlReportGeneratorService.java
@@ -0,0 +1,10 @@
+package work2.services;
+
+public class XmlReportGeneratorService implements XmlReportGenerator {
+    @Override
+    public String generateXml() {
+        String report = "";
+        // todo Logic
+        return report;
+    }
+}
