ac9aab0333106abd1bd9c0773758f055521bd309
arthursae <118846805+arthursae@users.noreply.github.com>
2023-07-28 20:08:00 +0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+import Work3.Greeter;
+import Work3.Greeting;
+
+public class Main {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter(Greeting.FORMAL);
+        System.out.println(greeter.greet());
+        greeter.setFormality(Greeting.CASUAL);
+        System.out.println(greeter.greet());
+        greeter.setFormality(Greeting.INTIMATE);
+        System.out.println(greeter.greet());
+        greeter.setFormality(Greeting.DEFAULT);
+        System.out.println(greeter.greet());
+    }
+}
