92f0fcd953a58609ce10e000cb8c89bee8d7473f
arthursae <118846805+arthursae@users.noreply.github.com>
2023-07-28 20:10:02 +0500

    Create Rectangle.java

---
+++ work1/Rectangle.java
@@ -0,0 +1,12 @@
+package work1;
+
+// Прямоугольник
+public class Rectangle {
+
+    int height;
+    int width;
+
+    public void setHeight(int height) { this.height = height; }
+
+    public void setWidth(int width) { this.width = width; }
+}
