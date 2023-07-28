3507b825c51f7ebb1df5cf6adc6b0f2d37df01e3
arthursae <118846805+arthursae@users.noreply.github.com>
2023-07-28 20:16:01 +0500

    Create JsonReportGeneratorService.java

---
+++ work2/services/JsonReportGeneratorService.java
@@ -0,0 +1,13 @@
+package work2.services;
+
+public class JsonReportGeneratorService implements JsonReportGenerator {
+
+    public JsonReportGeneratorService() {
+    }
+
+    public String generateJson() {
+        String report = "";
+        // todo Logic
+        return report;
+    }
+}
